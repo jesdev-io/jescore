d55d8ff4c4e527d5c944a1434caf1eae7efd2fb7
jake-is-ESD-protected <jt@tschavoll.at>
2023-10-11 10:55:17 +0200

    move user jobs to main for example clarity

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,29 @@
 #include "jescore.h"
 #include "user_jobs.h"
 
+void stats(void* p){
+
+}
+
+void help(void* p){
+    
+}
+
+
+void led(void* p){
+    pinMode(27, OUTPUT);
+    static bool act = true;
+    if(act){
+        digitalWrite(27, HIGH);
+    }
+    else{
+        digitalWrite(27, LOW);
+    }
+    act = !act;
+    vTaskDelete(NULL);
+}
+
+
 void setup() {
     jes_init();
 
