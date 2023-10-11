7c3bc2b2fd2230360eb2f8f4613e6ced47014c8f
jake-is-ESD-protected <jt@tschavoll.at>
2023-10-11 19:22:45 +0200

    change docstyle

--- lib/commands/commands.h
+++ lib/commands/commands.h
@@ -3,9 +3,8 @@
 
 #include <Arduino.h>
 
-/*@brief Enum for types of places where a command can
-* originate from. Used in cmd_struct_t
-*/
+/// @brief Enum for types of places where a command can
+/// originate from. Used in command_struct_t.
 typedef enum origin_t{
     e_origin_undefined,
     e_origin_interrupt,
@@ -13,24 +12,17 @@ typedef enum origin_t{
     e_origin_core
 }origin_t;
 
-/*@brief Enum for command content types aka jobs to do.
-* Used in cmd_struct_t
-*/
+/// @brief Enum for command content types aka jobs to do.
+/// Used in cmd_struct_t.
 typedef enum cmd_t{
     e_cmd_undefined,
     e_cmd_generic,
     e_cmd_read_queue
 }cmd_t;
 
-/*@brief Struct for sendable commands
-* @param origin (origin_t): Place of command creation
-* @param content (cmd_t): The job to do
-* @param o_value (uint16_t): Optional 16 bit value
-* @note Although not certain, it is assumed that both origin and
-* content are 8 bit wide, which makes the entire struct 32 bit
-* wide, small enough for atomic access and able to be passed along
-* wirh FreeRTOS task notifications
-*/
+/// @brief Struct for sendable commands.
+/// @param origin (origin_t): Place of command creation.
+/// @param content (cmd_t): The job to do.
 typedef struct cmd_struct_t{
     origin_t origin = e_origin_undefined;
     cmd_t content = e_cmd_undefined;
