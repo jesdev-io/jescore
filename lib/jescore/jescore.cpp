8eadd184f62d5d7ed436dc9d7668d44ed7aa3c6a
jake-is-ESD-protected <jt@tschavoll.at>
2023-10-24 14:40:30 +0200

    refactor

--- lib/jescore/jescore.cpp
+++ lib/jescore/jescore.cpp
@@ -13,7 +13,7 @@ err_t register_job(const char* name,
                    uint32_t mem_size,
                    uint8_t priority,
                    void (*function)(void* p)){
-    return __core_register_job(name,
+    return __job_register_job(name,
                                mem_size,
                                priority,
                                function);
@@ -21,14 +21,13 @@ err_t register_job(const char* name,
 
 
 err_t launch_job(const char* name){
-    return __core_launch_job_by_name(name);
+    return __job_launch_job_by_name(name);
 }
 
 
 err_t to_printer(const char* s){
-    job_struct_t** job_list = __core_get_job_list();
-    job_struct_t* printer = __job_get_job_by_name(job_list, PRINT_JOB_NAME);
-    job_struct_t* core_job = __job_get_job_by_name(job_list, CORE_JOB_NAME);
+    job_struct_t* printer = __job_get_job_by_name(PRINT_JOB_NAME);
+    job_struct_t* core_job = __job_get_job_by_name(CORE_JOB_NAME);
     uint8_t i = 0;
     while(s[i] != '\0' && i < __MAX_JOB_NAME_LEN_BYTE){
         printer->args[i] = s[i];
