a160407b771a37a18ebba1118bc3fa04e344515d
jake-is-ESD-protected <jt@tschavoll.at>
2023-12-05 15:23:44 +0100

    switch Serial for uart_unif

--- lib/base_jobs/base_jobs.cpp
+++ lib/base_jobs/base_jobs.cpp
@@ -1,11 +1,16 @@
 #include "base_jobs.h"
 #include "job_driver.h"
+#include "uart.h"
+#include "string.h"
 
 
 
 void __base_job_echo(void* p){
     job_struct_t* pj = __job_get_job_by_func(__base_job_echo);
-    Serial.println(pj->args);
+    uint8_t* msg = (uint8_t*)(pj->args);
+    char buf[64] = {0};
+    sprintf(buf, "%s\n", pj->args);
+    uart_unif_write((uint8_t*)buf, strlen(buf));
 }
 
 
