0db85239c09e1a6406eec861e62e6f85396a5f5e
jake-is-ESD-protected <jt@tschavoll.at>
2024-03-03 23:37:39 +0100

    add stats and name jobs

--- lib/base_jobs/base_jobs.cpp
+++ lib/base_jobs/base_jobs.cpp
@@ -2,11 +2,12 @@
 #include "job_driver.h"
 #include "uart.h"
 #include "string.h"
-
+#include "job_names.h"
+#include "core.h"
 
 
 void __base_job_echo(void* p){
-    job_struct_t* pj = __job_get_job_by_func(__base_job_echo);
+    job_struct_t* pj = (job_struct_t*)p;
     uint8_t* msg = (uint8_t*)(pj->args);
     char buf[64] = {0};
     sprintf(buf, "%s\n", pj->args);
@@ -14,3 +15,72 @@ void __base_job_echo(void* p){
 }
 
 
+jes_err_t __base_job_echo_wrapper(const char* s, origin_t o){
+    job_struct_t* printer = __job_get_job_by_name(PRINT_JOB_NAME);
+    job_struct_t* core_job = __job_get_job_by_name(CORE_JOB_NAME);
+    jes_err_t stat = __job_copy_str(printer->args, (char*)s, __MAX_JOB_ARGS_LEN_BYTE);
+    if(stat != e_err_no_err){ return stat; }
+    printer->caller = o;
+    __job_notify(core_job, printer, false);
+    return e_err_no_err;
+}
+
+
+void __base_job_help(void* p){
+    char desc[__MAX_JOB_ARGS_LEN_BYTE*2] = {0};
+
+    job_struct_t** job_list = __core_get_job_list();
+    job_struct_t* cur = *job_list;
+
+    sprintf(desc, "\x1b[1mAvailable jobs:\x1b[0m\n\r");
+    uart_unif_write((uint8_t*)desc, strlen(desc));
+    while(cur != NULL){
+        if(cur->role == e_role_base){
+            sprintf(desc, "\t\t(base) %s\n\r", cur->name);
+            uart_unif_write((uint8_t*)desc, strlen(desc));
+        }
+        else if(cur->role == e_role_user){
+            sprintf(desc, "\t\t(user) %s\n\r", cur->name);
+            uart_unif_write((uint8_t*)desc, strlen(desc));
+        }
+        cur = cur->pn;
+    }
+}
+
+
+void __base_job_stats(void* p){
+    char desc[__MAX_JOB_ARGS_LEN_BYTE*2] = {0};
+    char spacing_name[] = {'\t', 0, 0};
+    char spacing_addr[] = {'\t', 0, 0};
+
+    job_struct_t** job_list = __core_get_job_list();
+    job_struct_t* cur = *job_list;
+
+    sprintf(desc, "\x1b[1mname\t\thandle\t\tmemory\tprio\tloop\tinstances\x1b[0m\n\r");
+    uart_unif_write((uint8_t*)desc, strlen(desc));
+    while(cur != NULL){
+        if(strlen(cur->name) < 8){
+            spacing_name[1] = '\t';
+        }
+        else{
+            spacing_name[1] = 0;
+        }
+        if(cur->handle == NULL){
+            spacing_addr[1] = '\t';
+        }
+        else{
+            spacing_addr[1] = 0;
+        }
+        sprintf(desc, "\t\t%s%s%x%s%d\t%d\t%d\t%d\n\r", 
+                cur->name, 
+                spacing_name,
+                cur->handle, 
+                spacing_addr,
+                cur->mem_size, 
+                cur->priority,
+                cur->is_loop,
+                cur->instances);
+        uart_unif_write((uint8_t*)desc, strlen(desc));
+        cur = cur->pn;
+    }
+}
--- lib/base_jobs/base_jobs.h
+++ lib/base_jobs/base_jobs.h
@@ -1,6 +1,15 @@
 #ifndef _BASE_JOBS_H_
 #define _BASE_JOBS_H_
 
+#include "commands.h"
+#include "err.h"
+
 void __base_job_echo(void* p);
+
+jes_err_t __base_job_echo_wrapper(const char* s, origin_t o);
+
+void __base_job_help(void* p);
+
+void __base_job_stats(void* p);
 
 #endif
