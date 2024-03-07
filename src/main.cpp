8ac83ae3b62f407efdd3a33e225512fd9250f01b
jake-is-ESD-protected <jt@tschavoll.at>
2024-03-07 22:19:21 +0100

    cleanup main example

--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,29 @@
 #include "jescore_api.h"
 
 #define MAX_JOB_ARGS_LEN_BYTE 64
+#define LED_PIN 4
 
-#ifdef BUILTIN_LED
-#define LED_PIN BUILTIN_LED
-#else
-#define LED_PIN 2
-#endif
-
-bool act = false;
 
 void led_init(void* p){
     pinMode(LED_PIN, OUTPUT);
-    act = true;
 }
 
-void led_deinit(void* p){
-    act = false;
-}
 
 void blink(void* p){
-    while(1){
-        if(act){
-            digitalWrite(LED_PIN, HIGH);
-            delay(1000);
-            digitalWrite(LED_PIN, LOW);
-            delay(1000);
-        }
-        else{
-            return;
-        }
+    static bool act = false;
+    act = !act;
+    while(act){
+        digitalWrite(LED_PIN, HIGH);
+        delay(1000);
+        digitalWrite(LED_PIN, LOW);
+        delay(1000);
     }
 }
-
 
 
 void setup() {
     jes_init();
     register_and_launch_job("ledinit", 2048, 1, led_init, false);
-    register_job("leddeinit", 1024, 1, led_deinit, false);
     register_and_launch_job("blink", 2048, 1, blink, true);
 }
 
