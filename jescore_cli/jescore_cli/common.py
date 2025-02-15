e9cfd0b8503b128442df90d6523171a0b9563b04
jake-is-ESD-protected <jt@tschavoll.at>
2025-02-15 16:35:55 +0100

    client side testing

--- jescore_cli/jescore_cli/common.py
+++ jescore_cli/jescore_cli/common.py
@@ -21,7 +21,7 @@ KNOWN_HOSTS =  {"Electrosmith Daisy Seed": "VID:PID=0483:5740",
                 "Generic board with CH340 USB-to-UART converter": "VID:PID=1A86:7523",
                 "USB enhanced serial CH343": "VID:PID=1A86:55D3"}
 CLI_PREFIX_CLIENT = "[jes-core]:\t"
-CLI_PREFIX_MCU = "jes-core $"
+CLI_PREFIX_MCU = "jes-core $ "
 CLI_CLIENT_IDENTIFIER = "@py" # unused
 
 
