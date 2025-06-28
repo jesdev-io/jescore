763d81e8119626b9b698cb45934d07322d3c1f74
jake-is-ESD-protected <jt@tschavoll.at>
2025-06-28 18:23:39 +0200

    stm32: add STLINK to known PIDs/VIDs

--- jescore_cli/jescore_cli/common.py
+++ jescore_cli/jescore_cli/common.py
@@ -19,7 +19,8 @@ General baseline constants
 """
 KNOWN_HOSTS =  {"Electrosmith Daisy Seed": "VID:PID=0483:5740",
                 "Generic board with CH340 USB-to-UART converter": "VID:PID=1A86:7523",
-                "USB enhanced serial CH343": "VID:PID=1A86:55D3"}
+                "USB enhanced serial CH343": "VID:PID=1A86:55D3",
+                "ST LINK": "VID:PID=0483:374B"}
 CLI_PREFIX_CLIENT = "[jescore]:\t"
 CLI_PREFIX_MCU = "jescore $ "
 CLI_CLIENT_IDENTIFIER = "@py" # unused
