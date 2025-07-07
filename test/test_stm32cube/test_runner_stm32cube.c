9f78af7a174fe7d4218cf012ab8eba2c149a739d
jake-is-ESD-protected <jt@tschavoll.at>
2025-07-07 17:18:06 +0200

    stm32: platform-agnostic testing config

---
+++ test/test_stm32cube/test_runner_stm32cube.c
@@ -0,0 +1,16 @@
+/**
+ * @file test_runner.c
+ * @author jake-is-ESD-protected
+ * @date 2025-06-29
+ * @brief Main test runner file.
+ *
+ *
+ */
+
+
+#include "../common_test.h"
+
+int main(void){
+    test_all();
+    return 0;
+}
