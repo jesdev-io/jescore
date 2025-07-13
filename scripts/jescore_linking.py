0984a21563c24ecb4b61c7892158f9072ae20c38
jake-is-ESD-protected <jt@tschavoll.at>
2025-07-13 12:41:06 +0200

    fix: script formating

--- scripts/jescore_linking.py
+++ scripts/jescore_linking.py
@@ -50,70 +50,70 @@ vendor = board.get("build.vendor", "").lower()  # e.g., "st"
 print(f"Starting dynamic linking script for jescore on {Color.G + mcu + Color.X} ...")
 # ESP32 specific linking is done out-of-the-box
 if "esp" in mcu:
-   print(" > Found ESP32 platform, defaulting to base config ...")
+    print(" > Found ESP32 platform, defaulting to base config ...")
 
 else:
-   # Determine STM32 family (e.g., "stm32l4", "stm32f4")
-   stm_family = mcu[:7].lower() if mcu.startswith("stm32") else "unknown"
+    # Determine STM32 family (e.g., "stm32l4", "stm32f4")
+    stm_family = mcu[:7].lower() if mcu.startswith("stm32") else "unknown"
 
-   freertos_base = f"$PROJECT_PACKAGES_DIR/framework-stm32cube{stm_family[5:7]}"  # e.g., "l4", "f4"
+    freertos_base = f"$PROJECT_PACKAGES_DIR/framework-stm32cube{stm_family[5:7]}"  # e.g., "l4", "f4"
 
-   if "cortex-m4" in core:
-      freertos_port_path = f"{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
-   elif "cortex-m33" in core:
-      freertos_port_path = f"{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure"
-   else:
-      print(Color.R + f"Warning: Unsupported core {core} for FreeRTOS" + Color.X)
-      freertos_port_path = None
+    if "cortex-m4" in core:
+        freertos_port_path = f"{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+    elif "cortex-m33" in core:
+        freertos_port_path = f"{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure"
+    else:
+        print(Color.R + f"Warning: Unsupported core {core} for FreeRTOS" + Color.X)
+        freertos_port_path = None
 
-   jescore_as_lib = os.path.join(env["PROJECT_LIBDEPS_DIR"], env["PIOENV"], "jescore")
-   if os.path.exists(jescore_as_lib):
-      config_path = os.path.join(  
-         jescore_as_lib,
-         "include", 
-         "FreeRTOSConfig",  
-         "FreeRTOSConfig.h"  
-      )  
-      env.Append(CPPPATH=[os.path.dirname(config_path)])  
-      print(f"Added STM32 FreeRTOSConfig.h from: {config_path}")  
-   else:
-      env.Append(CPPPATH=["$PROJECT_DIR/include/FreeRTOSConfig"]) 
-      print("Local build of library...")
+    jescore_as_lib = os.path.join(env["PROJECT_LIBDEPS_DIR"], env["PIOENV"], "jescore")
+    if os.path.exists(jescore_as_lib):
+        config_path = os.path.join(  
+            jescore_as_lib,
+            "include", 
+            "FreeRTOSConfig",  
+            "FreeRTOSConfig.h"  
+        )  
+        env.Append(CPPPATH=[os.path.dirname(config_path)])  
+        print(f"Added STM32 FreeRTOSConfig.h from: {config_path}")  
+    else:
+        env.Append(CPPPATH=["$PROJECT_DIR/include/FreeRTOSConfig"]) 
+        print("Local build of library...")
 
-   mcu_flags = [
-      "-mfloat-abi=hard",
-      "-mfpu=fpv4-sp-d16",
-      f"-mcpu={core}",
-      "-mthumb",
-      "-DUSE_HAL_DRIVER",
-   ]
+    mcu_flags = [
+        "-mfloat-abi=hard",
+        "-mfpu=fpv4-sp-d16",
+        f"-mcpu={core}",
+        "-mthumb",
+        "-DUSE_HAL_DRIVER",
+    ]
 
-   env.Append(
-      CPPPATH=[
-         f"{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/include",
-         f"{freertos_port_path}" if freertos_port_path else "",
-      ],
-      CCFLAGS=mcu_flags,
-      CXXFLAGS=mcu_flags,
-      LINKFLAGS=[
-         "-mfloat-abi=hard",
-         "-mfpu=fpv4-sp-d16"
-      ],
+    env.Append(
+        CPPPATH=[
+            f"{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/include",
+            f"{freertos_port_path}" if freertos_port_path else "",
+        ],
+        CCFLAGS=mcu_flags,
+        CXXFLAGS=mcu_flags,
+        LINKFLAGS=[
+            "-mfloat-abi=hard",
+            "-mfpu=fpv4-sp-d16"
+        ],
 
-      SRC_FILTER=[
-         "+<**/*.c>",
-         f"+<{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/*.c>",
-         f"+<{freertos_port_path}/*.c>" if freertos_port_path else "",
-         f"+<{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c>",
-      ],
-   )
+        SRC_FILTER=[
+            "+<**/*.c>",
+            f"+<{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/*.c>",
+            f"+<{freertos_port_path}/*.c>" if freertos_port_path else "",
+            f"+<{freertos_base}/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c>",
+        ],
+    )
 
-   env.AppendUnique(
-      BUILD_FLAGS=mcu_flags
-   )
+    env.AppendUnique(
+        BUILD_FLAGS=mcu_flags
+    )
 
-   print(f"MCU: {Color.G + mcu + Color.X}, Core: {Color.G + core + Color.X}, STM Family: {Color.G + stm_family + Color.X}")
-   print(f"FreeRTOS Path: {Color.G + freertos_port_path + Color.X}")
+    print(f"MCU: {Color.G + mcu + Color.X}, Core: {Color.G + core + Color.X}, STM Family: {Color.G + stm_family + Color.X}")
+    print(f"FreeRTOS Path: {Color.G + freertos_port_path + Color.X}")
 
 
 
