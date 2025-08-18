34838d3211562e0b528f9e4c497fde383c41d98d
jake-is-ESD-protected <j.tschavoll@tu-berlin.de>
2025-08-18 13:14:57 +0200

    device: add CP2102 to known connections

--- jescore_cli/jescore_cli/common.py
+++ jescore_cli/jescore_cli/common.py
@@ -17,7 +17,8 @@ General baseline constants
 `CLI_CLIENT_IDENTIFIER`:        Prefix for the transmitted message to distinguish
                                 between this CLI and the headless CLI.
 """
-KNOWN_HOSTS =  {"Electrosmith Daisy Seed": "VID:PID=0483:5740",
+KNOWN_HOSTS =  {"Generic board with CP2102 USB-to-UART converter": "VID:PID=10C4:EA60",
+                "Electrosmith Daisy Seed": "VID:PID=0483:5740",
                 "Generic board with CH340 USB-to-UART converter": "VID:PID=1A86:7523",
                 "USB enhanced serial CH343": "VID:PID=1A86:55D3",
                 "ST LINK": "VID:PID=0483:374B",
