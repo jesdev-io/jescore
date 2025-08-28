8f70a93bed3def685ac37be3f4986fa98774b3a4
jake-is-ESD-protected <j.tschavoll@tu-berlin.de>
2025-08-28 11:36:39 +0200

    Board: add USB Single Serial (for e.g. the ESP32-WROVER)

--- jescore_cli/jescore_cli/common.py
+++ jescore_cli/jescore_cli/common.py
@@ -22,7 +22,8 @@ KNOWN_HOSTS =  {"Generic board with CP2102 USB-to-UART converter": "VID:PID=10C4
                 "Generic board with CH340 USB-to-UART converter": "VID:PID=1A86:7523",
                 "USB enhanced serial CH343": "VID:PID=1A86:55D3",
                 "ST LINK": "VID:PID=0483:374B",
-                "ST LINK V3": "VID:PID=0483:374E"}
+                "ST LINK V3": "VID:PID=0483:374E",
+                "USB Single Serial": "USB VID:PID=1A86:55D4"}
 CLI_PREFIX_CLIENT = "[jescore]:\t"
 CLI_PREFIX_MCU = "jescore $ "
 CLI_CLIENT_IDENTIFIER = "@py" # unused
