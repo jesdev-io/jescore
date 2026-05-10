9df07ffb8bc258cc59170b6d5141b5cc46d7769c
jake-is-ESD-protected <jt@tschavoll.at>
2026-05-10 17:08:42 +0200

    fix: possible null deref

--- lib/job_driver/job_driver.c
+++ lib/job_driver/job_driver.c
@@ -155,8 +155,8 @@ jes_err_t __job_unregister_job(const char* n){
 
 jes_err_t __job_launch_job(job_struct_t* pj, origin_t o){
     BaseType_t stat;
-    pj->caller = o;
     if(pj == NULL){ return e_err_unknown_job; }
+    pj->caller = o;
     if((o == e_origin_cli || o == e_origin_api) && pj->role == e_role_core){
         // Prohibit the calling of core jobs from API and CLI
         return e_err_prohibited;
